d44c6bc999117f73a146551d47efcc80ba1faae8
Aiden <aiden@djkraz.com>
2023-07-17 07:29:01 -0500

    fixed 404 and made webamp disable on mobile

--- scripts/webamp.js
+++ scripts/webamp.js
@@ -1,47 +1,47 @@
-const Webamp = window.Webamp;
-const player = new Webamp({
-  initialTracks: [
-    {
-      metaData: {
-        artist: "Ketsa",
-        title: "Caught Out",
+if (window.innerWidth > window.innerHeight/1.2) {
+  const Webamp = window.Webamp;
+  const player = new Webamp({
+    initialTracks: [
+      {
+        metaData: {
+          artist: "Ketsa",
+          title: "Caught Out",
+        },
+        url: "music/co.mp3",
+        duration: 180,
       },
-      url: "music/co.mp3",
-      duration: 180
-    },
-    {
-      metaData: {
-        artist: "Aldous Ichnite",
-        title: "Nebulous Twilight",
+      {
+        metaData: {
+          artist: "Aldous Ichnite",
+          title: "Nebulous Twilight",
+        },
+        url: "music/nt.mp3",
+        duration: 110,
       },
-      url: "music/nt.mp3",
-      duration: 110
-    },
-    {
-      metaData: {
-        artist: "Origami Repetika",
-        title: "From The Sparkles In Your Eyes",
+      {
+        metaData: {
+          artist: "Origami Repetika",
+          title: "From The Sparkles In Your Eyes",
+        },
+        url: "music/ftsiye.mp3",
+        duration: 186,
       },
-      url: "music/ftsiye.mp3",
-      duration: 186
-    },
-    {
-      metaData: {
-        artist: "Ketsa",
-        title: "Bustle Beat",
+      {
+        metaData: {
+          artist: "Ketsa",
+          title: "Bustle Beat",
+        },
+        url: "music/bb.mp3",
+        duration: 184,
       },
-      url: "music/bb.mp3",
-      duration: 184
-    },
-    {
-      metaData: {
-        artist: "Ketsa",
-        title: "Who Needs Saving",
+      {
+        metaData: {
+          artist: "Ketsa",
+          title: "Who Needs Saving",
+        },
+        url: "music/wns.mp3",
+        duration: 191,
       },
-      url: "music/wns.mp3",
-      duration: 191
-    },
-  ],
-}).renderWhenReady(document.getElementById("app"));
-
-player.play(); // Autoplay the first track
+    ],
+  }).renderWhenReady(document.getElementById("app"));
+}
